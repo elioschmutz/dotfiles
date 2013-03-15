4022f7875a951eef299b43183d04ebc648f9cdc0
Elio Schmutz <elio.schmutz@4teamwork.ch>
2013-03-15 10:32:09 +0100

    Update dotfiles

---
+++ .emacs.d/init.el
@@ -0,0 +1,2 @@
+(setq cabbage-repository (expand-file-name "/Users/elioschmutz/Plone/projects/cabbage/"))
+(load (concat cabbage-repository "cabbage"))
---
+++ .emacs.d/org-clock-save.el
@@ -0,0 +1,1 @@
+;; org-persist.el - 4teamworks-MacBook-Pro at <2013-03-12 Tue 09:04>
